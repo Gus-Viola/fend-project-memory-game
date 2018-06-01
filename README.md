4e1c43fd417f71c4b565d3e173a413300f6f2cdd
Gus Viola <gus.viola@pobox.com>
2018-06-01 20:25:08 -0300

    Fixed modals, first click issue, README and basically all that was left.

--- README.md
+++ README.md
@@ -21,33 +21,7 @@ The game is over when all the cards have been matched.the project instructions i
 
 ## Dependencies
 
-[Font Awesome](https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css)
-[Google Fonts](https://fonts.googleapis.com/css?family=Coda)
-[Animate.CSS](https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css)
-[JQuery](https://code.jquery.com/jquery-3.2.1.min.js)
-
-Dependency is a broad software engineering term used to refer when a piece of software relies on another one. Coupling (computer programming) In software engineering, coupling or dependency is the degree to which each program module relies on each one of the other modules.
-https://askubuntu.com/questions/361741/what-are-dependencies
-Nov 9, 2013
-
-## Rubric
-
-#Game Behavior
-
-
-Congratulations Popup
-
-When a user wins the game, a modal appears to congratulate the player and ask if they want to play again. It should also tell the user how much time it took to win the game, and what the star rating was.
-
-Star Rating
-
-The game displays a star rating (from 1 to at least 3) that reflects the player's performance. At the beginning of a game, it should display at least 3 stars. After some number of moves, it should change to a lower star rating. After a few more moves, it should change to a even lower star rating (down to 1). The number of moves needed to change the rating is up to you, but it should happen at some point.
-
-
-Move Counter
-
-Game displays the current number of moves a user has made.
-
-Code Quality
-
-Code is formatted with consistent, logical, and easy-to-read formatting as described in the Udacity JavaScript Style Guide.
+Font Awesome [link](https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css)
+Google Fonts [link](https://fonts.googleapis.com/css?family=Coda)
+Animate.CSS [link](https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css)
+JQuery [link](https://code.jquery.com/jquery-3.2.1.min.js)
--- js/app.js
+++ js/app.js
@@ -1,20 +1,6 @@
-/*
- * Gus: Declare variables here, too.
- openCards are the one or two cards currently in play  OK
- revealedCards are the cards that have previously been revealed
- openShow are the cards on constant display because of matching pairs;
- giantArray because 500 icons are better than eight
+let arrayofCards, openCards, openShow, moveCounter, giantArray;
+let starDemerits, timerOn, timer, secondsLeft, firstClick;
 
-brush up README file
-restart on onload
-adjust Modal
-Optional:
-Add CSS animations when cards are clicked, unsuccessfully matched, and successfully matched.
-Add unique functionality beyond the minimum requirements (Implement a leaderboard, store game state using local storage, etc.)
-Implement additional optimizations that improve the performance and user experience of the game (keyboard shortcuts for gameplay, etc).
-
- */
-let arrayofCards, openCards, openShow, moveCounter, giantArray, starDemerits, timerOn, timer, secondsLeft;
 arrayofCards = document.querySelectorAll(".card"); //list that holds all cards
 openCards = [];
 openShow = [];
@@ -24,100 +10,80 @@ scoreStars = document.querySelectorAll(".fa-star");
 starDemerits = 0;
 timerOn = false;
 secondsLeft = 45;
+firstClick = true;
 
 $(".restart").click(function() {
-  // if (timerOn) {
-  //   return null;
-  // }
   displayCards();
   $(".card").click(onCardClick); //event handler needs to be recreated after displayCards run
-  moveCounter = 0; //reset
+  moveCounter = 0;
   document.querySelector(".moves").innerHTML = moveCounter.toString();
   arrayofCards = document.querySelectorAll(".card"); //list that holds all cards
 
   openCards = [];
   openShow = [];
-  moveCounter = 0;
   giantArray = document.querySelectorAll(".newCard");
   scoreStars = document.querySelectorAll(".fa-star");
-  //  resetStars();
-  /*scoreStars.forEach(function(obj) {
-    $(obj).animateCss("flip", function() {
-      $(obj).addClass("checked");
-    });
-  });//end of forEach loop*/
 
   starDemerits = 0;
   changeStarScore();
-  timerFunction(); //change to first card touched
+  if (timerOn) {
+    clearInterval(timer);
+  }
+  timerOn = false;
+  firstClick = true;
+
 });
 
-
-// <div id="timer">00:00:00</div>
 
 let message = document.getElementById("timer");
 
 // https://gist.github.com/vivekrk/3918717
-function timerFunction () { //timer is broken. clicks restart multiple times wrecks it
-    if (timerOn) {
-      clearInterval(timer);
+function timerFunction() {
+  if (timerOn) {
+    clearInterval(timer);
+  }
+  timerOn = true;
+  secondsLeft = 45;
+  timer = setInterval(function() {
+    message.innerHTML = secondsLeft + " seconds";
+    secondsLeft--;
+    if (secondsLeft === 30 || secondsLeft === 15) {
+      starDemerits++;
+      changeStarScore();
     }
-    timerOn = true;
-    secondsLeft = 45;
-    timer = setInterval(function() {
-        message.innerHTML = secondsLeft + " seconds";
-        secondsLeft--;
-        if (secondsLeft === 30 || secondsLeft === 15) {
-          starDemerits++;
-          changeStarScore();
-        }
 
-        if(secondsLeft === 0) {
-            clearInterval(timer);
-            starDemerits++;
-            changeStarScore();
-            message.innerHTML = "Time's up!"
-            timerOn = false;
-            displayWinModal("failure"); //any text <> "success" will do
-        }
-    }, 1000);
+    if (secondsLeft === 0) {
+      clearInterval(timer);
+      starDemerits++;
+      changeStarScore();
+      message.innerHTML = "Time's up!"
+      timerOn = false;
+      endGame();
+      displayFailModal();
+    }
+  }, 1000);
 }
 
-// var timer = new Timer();
-function changeStarScore(){
+function changeStarScore() {
   if (starDemerits === 0) {
     scoreStars.forEach(function(obj) {
       $(obj).animateCss("flip", function() {
         $(obj).addClass("checked");
       });
-    });//end of forEach loop
-  }//if 0
-/*  if (starDemerits === 1) {
-    obj = scoreStars[3-starDemerits];
-    $(obj).animateCss("flip", function() {
-      $(obj).removeClass("checked");
-    });
-  }//if 1
-  if (starDemerits === 2) {
-    obj = scoreStars[3-starDemerits];
-    $(obj).animateCss("flip", function() {
-      $(obj).removeClass("checked");
-    });
-  }//if 2
-  if (starDemerits === 3) */
+    }); //end of forEach loop
+  }
   else {
-    obj = scoreStars[3-starDemerits];
+    obj = scoreStars[3 - starDemerits];
     $(obj).animateCss("flip", function() {
       $(obj).removeClass("checked");
     });
-  }//else
+  }
 }
 
 
 function displayCards() {
   let tempArray = [];
-  //tempArray = shuffle([...arrayofCards]); //spread allows for correct array manipulation
-  tempArray = shuffle([...giantArray]).slice(0,8);
+  tempArray = shuffle([...giantArray]).slice(0, 8);
   tempArray = tempArray.concat(tempArray);
   tempArray = shuffle([...tempArray]);
   $(".deck").children().remove();
@@ -130,7 +96,7 @@ function displayCards() {
     newItem.className = $(arrayElement).children().attr("class");
     newListItem.appendChild(newItem);
     document.querySelector(".deck").appendChild(newListItem);
-  });//end of forEach loop
+  }); //end of forEach loop
   return null;
 } //end of displayCards()
 
@@ -148,29 +114,17 @@ function shuffle(array) {
   }
 
   return array;
-}// end of shuffle(array)
+} // end of shuffle(array)
 
-/*
- * DONE set up the event listener for a card. If a card is clicked:
- * DONE - display the card's symbol (put this functionality in another function that you call from this one) DONE
- * DONE - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- * DONE - if the list already has another card, check to see if the two cards match
- * DONE DIFFERENTLY   + if the cards do match, lock the cards in the open position (put this functionality in another function
-  that you call from this one)
- * DONE DIFFERENTLY   + if the cards do not match, remove the cards from the list and hide the card's symbol (put this
- functionality in another function that you call from this one)
- * DONE   + increment the move counter and display it on the page (put this functionality in another function
- that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in
- another function that you call from this one)
-Gus: Star system will have demerits if turned card has data-revealed = true
-
- *
-*/
 
 $(".card").click(onCardClick);
 
 function onCardClick() {
+  if (firstClick) {
+    firstClick = false;
+    timerFunction();
+  }
+
   $(this).attr("data-revealed", "true"); //for the star demerits
   if ($(this).hasClass("open show") || $(this).hasClass("match")) { //if card is open, clicking it does nothing
     return null;
@@ -179,18 +133,18 @@ function onCardClick() {
   if (openCards.length < 2) {
     $(this).addClass("open show");
     openCards.push($(this));
-    moveCounter++;
-    document.querySelector(".moves").innerHTML = moveCounter.toString();
   }
 
   if (openCards.length === 2) {
+    moveCounter++;
+    document.querySelector(".moves").innerHTML = moveCounter.toString();
     checkOpenCards();
     openCards = [];
 
   }
 
   return null;
-}// end of onCardClick()
+} // end of onCardClick()
 
 
 function checkOpenCards() {
@@ -201,34 +155,37 @@ function checkOpenCards() {
       obj.animateCss("flip", function() {
         obj.removeClass("open show").addClass("match");
       });
-    });//end of forEach loop
+    }); //end of forEach loop
 
-  const string = openCards[0].children().attr("class"); //let to const
-  openShow.push(string); //openShow keeps track of winning cards
+    const string = openCards[0].children().attr("class"); //let to const
+    openShow.push(string); //openShow keeps track of winning cards
 
-  if (openShow.length == 8) {
-    endGame();
-  }
+    if (openShow.length === 8) {
+        endGame();
+        displayWinModal();
+    }
 
   } else { // cards do not match
     openCards.forEach(function(obj) {
       obj.animateCss("flipInX", function() {
         obj.removeClass("open show");
       });
-    }); //end of forEach loop //CODE HERE: must close cards and add star demerits
+    }); //end of forEach loop
   } //end of if-else
 
   return null;
-}//end of checkOpenCards()
+} //end of checkOpenCards()
 
 
 function endGame() {
   [...arrayofCards].forEach(function(obj) {
     $(obj).animateCss("wobble");
   }); //end of forEach loop
-  displayWinModal("success");
+
+  clearInterval(timer);
+
   return null;
-}// end of endGame()
+} // end of endGame()
 
 
 
@@ -240,35 +197,35 @@ function endGame() {
 const modal = document.getElementById("myModal");
 const modalContent = document.getElementsByClassName("modal-content");
 
-// Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+//const span = document.getElementsByClassName("close")[0];
 
 // Modal shows up when game is won or time is up
-function displayWinModal(string) {
 
+function displayFailModal() {
   modal.style.display = "block";
-  if (string == "success") {
-  const displayString = 'You have '+(3 - starDemerits)+' stars and still '+secondsLeft+' seconds left!';
-  $(modalContent).html('<h2>Congratulations, you have won!</h2><ul>'+displayString+'</ul><ul><li><h1><i class="fa fa-trophy"></i></h1></li></ul>');
-  $(modalContent).removeClass("failure").addClass("success"); }
-  else {
     $(modalContent).html('<h2>Sorry, your time is up!</h2><ul><li><h1><i class="fa fa-bomb"></i></h1></li></ul>');
-    $(modalContent).removeClass("success").addClass("failure"); }
+    $(modalContent).removeClass("success").addClass("failure");
   $(modal).animateCss("bounceInUp");
-
 }
+
+function displayWinModal() {
+  modal.style.display = "block";
+    const displayString = 'You have ' + (3 - starDemerits).toString() + ' stars and still ' + secondsLeft.toString() + ' seconds left!';
+    $(modalContent).html('<h2>Congratulations, you have won!</h2><ul>' + displayString + '</ul><ul><li><h1><i class="fa fa-trophy"></i></h1></li></ul>');
+  $(modalContent).removeClass("failure").addClass("success");
+  $(modal).animateCss("bounceInUp");
+}
+
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {  //can this die?
-    modal.style.display = "none";
-}
+//span.onclick = function() {
+//  modal.style.display = "none";
+//}
 
 // When the user clicks anywhere on the modal, close it
-window.onclick = function(event) {
-        modal.style.display = "none";
+window.onclick = function() {
+  modal.style.display = "none";
 }
 
 // code from https://github.com/daneden/animate.css/#usage
@@ -300,12 +257,5 @@ $.fn.extend({
 }); //end of jQuery extention for animation
 
 
-//this function will likely die
-// $.fn.redraw = function() {
-//   $(this).each(function() {
-//     var redraw = this.offsetHeight;
-//   });
-// };
-
-
-//document.body.onload = displayCards();
+document.body.onload = displayCards();
+document.body.onload = $(".card").click(onCardClick);
